8da3ae81b19f82c3d8aad7ac4eda809f798c18d4
Cian Hughes <86392453+AUsername4@users.noreply.github.com>
2021-08-02 12:30:03 +0800

    Commit 1

---
+++ .github/Main.py
@@ -0,0 +1,30 @@
+capnum == 0
+espnum == 0
+latnum == 0
+icenum == 0
+cupcount == 0
+totalsur == 0
+totalcost == 0
+
+print("""Todays coffe choices are:
+1: Cappuccino:   $3.00     
+2: Espresso:     $2.25
+3: Latte:        $2.50
+4: Iced Coffee:  $2.50
+""")
+e == int(input("Which coffee would you like to order?"))
+ordernum
+    cupcount = cupcount +1
+    if e = 1:
+        capnum = capnum +1
+        cost = (3*1.1)
+        surcharge = (3*0.1)
+        totalsur = totalsur + surcharge
+        totalcost = totalcosst + cost
+    elif e = 2:
+        espnum = espnum +1
+        cost = (2.25*1.1)
+        surcharge = (2.25*0.1)
+        totalsur = totalsur + surcharge
+        totalcost = totalcosst + cost
+    elif e
