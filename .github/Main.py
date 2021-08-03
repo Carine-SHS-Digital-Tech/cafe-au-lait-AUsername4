fbf7c2523a529350b8a39b12f68386c2ba5ef014
Cian Hughes <86392453+AUsername4@users.noreply.github.com>
2021-08-03 10:00:46 +0800

    commit 3

--- .github/Main.py
+++ .github/Main.py
@@ -1,35 +1,23 @@
-print("""Todays coffe choices are:
+P = 'E'
+while P == 'E':
+    neworsum = input("Would you like to place an order (1)? or view the daily summary? (2): ")
+    if neworsum == '1':
+        print("""Todays coffe choices are:
 1: Cappuccino:   $3.00     
 2: Espresso:     $2.25
 3: Latte:        $2.50
 4: Iced Coffee:  $2.50
 """)
-e == int(input("Which coffee would you like to order?"))
+        dict =  {1: 'Cappuccino', 2: 'Espresso', 3: 'Latte', 4: 'Iced Coffee'}
+        order = 'y'
+        while order.lower() == 'y':
+            order = input("Would you like to order (y) yes or (n) no?: ")
+            print("It worked!")
+        else:
+            print("End")
+    elif neworsum == '2':
+        print("THIS IS THE DAILY SUMMARY, COME BACK TO THIS LATER YOU NONCE")
+    else:
+        print("This is not a valid number")
 
-cupcount == cupcount +1
-if e == 1:
-    capnum = capnum +1
-    cost = (3*1.1)
-    surcharge = (3*0.1)
-    totalsur = totalsur + surcharge
-    totalcost = totalcosst + cost
-elif e == 2:
-    espnum = espnum +1
-    cost = (2.25*1.1)
-    surcharge = (2.25*0.1)
-    totalsur = totalsur + surcharge
-    totalcost = totalcosst + cost
-elif e == 3:
-    latnum = latnum +1
-    cost = (2.5*1.1)
-    surcharge = (2.5*0.1)
-    totalsur = totalsur + surcharge
-    totalcost = totalcosst + cost
-elif e == 4:
-    icenum = icenum +1
-    cost = (2.5*1.1)
-    surcharge = (2.5*0.1)
-    totalsur = totalsur + surcharge
-    totalcost = totalcosst + cost
-else:
-    print("Sorry that is not a valid order")
+
