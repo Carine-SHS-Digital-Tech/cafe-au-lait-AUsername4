232fed421ebf4dc49c5dcaed70d6d374840e72e9
Cian Hughes <86392453+AUsername4@users.noreply.github.com>
2021-08-03 09:12:44 +0800

    commit 2

--- .github/Main.py
+++ .github/Main.py
@@ -1,11 +1,3 @@
-capnum == 0
-espnum == 0
-latnum == 0
-icenum == 0
-cupcount == 0
-totalsur == 0
-totalcost == 0
-
 print("""Todays coffe choices are:
 1: Cappuccino:   $3.00     
 2: Espresso:     $2.25
@@ -13,18 +5,31 @@ print("""Todays coffe choices are:
 4: Iced Coffee:  $2.50
 """)
 e == int(input("Which coffee would you like to order?"))
-ordernum
-    cupcount = cupcount +1
-    if e = 1:
-        capnum = capnum +1
-        cost = (3*1.1)
-        surcharge = (3*0.1)
-        totalsur = totalsur + surcharge
-        totalcost = totalcosst + cost
-    elif e = 2:
-        espnum = espnum +1
-        cost = (2.25*1.1)
-        surcharge = (2.25*0.1)
-        totalsur = totalsur + surcharge
-        totalcost = totalcosst + cost
-    elif e
+
+cupcount == cupcount +1
+if e == 1:
+    capnum = capnum +1
+    cost = (3*1.1)
+    surcharge = (3*0.1)
+    totalsur = totalsur + surcharge
+    totalcost = totalcosst + cost
+elif e == 2:
+    espnum = espnum +1
+    cost = (2.25*1.1)
+    surcharge = (2.25*0.1)
+    totalsur = totalsur + surcharge
+    totalcost = totalcosst + cost
+elif e == 3:
+    latnum = latnum +1
+    cost = (2.5*1.1)
+    surcharge = (2.5*0.1)
+    totalsur = totalsur + surcharge
+    totalcost = totalcosst + cost
+elif e == 4:
+    icenum = icenum +1
+    cost = (2.5*1.1)
+    surcharge = (2.5*0.1)
+    totalsur = totalsur + surcharge
+    totalcost = totalcosst + cost
+else:
+    print("Sorry that is not a valid order")
